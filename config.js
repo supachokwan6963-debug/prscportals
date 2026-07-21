e3d184416d9b95d855b4b839fd7fa3b78bf349be
supachokwan6963-debug <supachok.wan6963@ndlp.go.th>
2026-07-21 21:52:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 const SUPABASE_URL = "https://dkuyxaujvhqanloqcgbh.supabase.co";
 
 // นำคีย์ที่เป็น anon public (ไม่ใช่ service_role) มาใส่ตรงนี้
-const SUPABASE_ANON_KEY = "วาง_ANON_KEY_ที่ได้จาก_Supabase_ตรงนี้";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrdXl4YXVqdmhxYW5sb3FjZ2JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyODg3OTcsImV4cCI6MjA5OTg2NDc5N30.GR2gCIWtqokqa5YBgXv1H1QsKQlPiDj9UF4i8C7BMmI";
